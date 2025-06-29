4a650d6e44c3ae2543707e42f1ab33387f82ddbf
Mario <mario.esquivel13795@gmail.com>
2025-06-28 23:44:56 -0400

    Add GitHub Actions workflow for automatic deployment

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import './Hero.css'
 import profile_img from '../../assets/profile3.jpg'
-import resume from '../../assets/Mario_s_Resume (2).pdf'
+import resume from '../../assets/Mario_s_Resume (1).pdf'
 
 const Hero = () => {
   return (
     <div id='hero'>
         <img src={profile_img} alt=''/>
         <h1><span>I'm Mario Esquivel,</span> Aspiring Software Engineer/Developer</h1>
-        <div className='hero-action'>
+        <div className='hero-action'> 
             <div className='hero-connect'>Connect with Me</div>
-            <a href={resume} id='hero-resume' download='Mario_s_Resume (2).pdf'>My Resume</a>
+            <a href={resume} id='hero-resume' download='Mario_s_Resume (1).pdf'>My Resume</a>
         </div>
     </div>
   )
--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -1,6 +1,7 @@
 import TaskManagerImg from '../assets/TaskManager_img.png'
 import mJobsImg from '../assets/m-jobsimg.png'
 import SplitViewImg from '../assets/Screenshot 2025-06-27 170544.png'
+import ifCalenderImg from '../assets/Screenshot 2025-06-28 230104.png'
 
 const mywork_data = [
     {
@@ -22,6 +23,13 @@ const mywork_data = [
         name: "Split View",
         img: SplitViewImg,
         github: "https://github.com/MarioEsquivelIII/active-tab-split",
+    },
+    {
+        no: 4,
+        name: "ifCalender",
+        img: ifCalenderImg,
+        github: "https://github.com/MarioEsquivelIII/ifCalender",
+        demo: "https://marioesquiveliii.github.io/ifCalender/",
     },
 ]
 
