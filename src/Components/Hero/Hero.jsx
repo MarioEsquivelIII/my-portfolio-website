946966fe19e9cf05012c7783ccc95f766c04ca33
Mario <mario.esquivel13795@gmail.com>
2025-06-30 16:38:11 -0400

    Updated Resume

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Hero.css'
 import profile_img from '../../assets/profile3.jpg'
-import resume from '../../assets/Mario_s_Resume (1).pdf'
+import resume from '../../assets/Mario_s_Resume (2).pdf'
 
 const Hero = () => {
   return (
@@ -10,7 +10,7 @@ const Hero = () => {
         <h1><span>I'm Mario Esquivel,</span> Aspiring Software Engineer/Developer</h1>
         <div className='hero-action'> 
             <div className='hero-connect'>Connect with Me</div>
-            <a href={resume} id='hero-resume' download='Mario_s_Resume (1).pdf'>My Resume</a>
+            <a href={resume} id='hero-resume' download='Mario_s_Resume (2).pdf'>My Resume</a>
         </div>
     </div>
   )
